d0607a3bf7fb7c0dcf2a1f79c93ce79cbf8eb9f5
Peskovatskov Ignat <113009998+Peskovatskow-Ignat@users.noreply.github.com>
2023-03-09 17:43:06 +0300

    update

--- Homework_2/Task_5.py
+++ Homework_2/Task_5.py
@@ -4,6 +4,7 @@ n - количество элементов в списке prices
 в худшем случае мы пройдемся по всем элементам списка два раза.
 """
 
+
 def getDescentPeriods(prices):
     n = len(prices)
     smooth_periods = n  # каждый элемент массива - период плавного спуска длины 1
@@ -12,10 +13,26 @@ def getDescentPeriods(prices):
     for i in range(1, n):
         j = i
         # ищем все последовательные дни с плавным спуском цены
-        while j < n and prices[j] == prices[j-1]-1:
+        while j < n and prices[j] == prices[j - 1] - 1:
             j += 1
         # добавляем количество периодов плавного спуска, найденных на текущей итерации
-        smooth_periods += j-i
+        smooth_periods += j - i
 
     # учитываем еще один период плавного спуска длины 1 - последний элемент массива
     return smooth_periods
+
+
+price = [3, 2, 1, 4]
+
+
+def foo(price):
+    dp = [1] * len(price)
+    s = 1
+    for i in range(1, len(price)):
+        if price[i - 1] - 1 == price[i]:
+            dp[i] = dp[i - 1] + 1
+        s += dp[i]
+    return s
+
+
+print(foo(price))
